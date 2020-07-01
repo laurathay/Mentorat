1d73408eddca4b9a108004fde848551b11af0647
Laura THAY <laura.thay@ipag.fr>
2020-07-01 09:20:57 +0200

    js ajouté

--- js-scroll-animation-master/javascript/index.js
+++ js-scroll-animation-master/javascript/index.js
@@ -1,1 +1,62 @@
+// function handleScroll() {
+//   let scrollFromTop = window.pageYOffset || document.documentElement.scrollTop;
+//   if (scrollFromTop > scrollPosition) {
+//     window.removeEventListener("scroll", handleScroll)
+//     window.scrollTo({
+//       top:1006,
+//       behavior: 'smooth',
+//     });
+//   }
+//
+//     console.log("scroolll", scrollFromTop);
+// }
+// window.addEventListener('scroll', handleScroll);
+//
+//
+//
+//
+//
+// let scrollPosition = 0 ;
+
+
 console.log('hello')
+
+
+var body = document.body,
+html = document.documentElement;
+
+const height = body.scrollHeight;
+console.log(height);
+
+let etape = 1
+
+let lastScrollTop = 0;
+function handleScroll() {
+    let st = window.pageYOffset || document.documentElement.scrollTop;
+    console.log(st);
+    if (st > lastScrollTop && etape == 1){
+      etape = 2
+    /*  window.removeEventListener("scroll", handleScroll)*/
+       window.scrollTo({
+        top: height/3,
+        left: 0,
+        behavior: 'smooth'
+      })
+    }else if (st < lastScrollTop && etape == 2){
+      etape = 1
+        window.scrollTo({
+         top: 0,
+         left: 0,
+         behavior: 'smooth'
+       })
+    } else {
+        console.log('up');
+    }
+    lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
+
+}
+
+window.addEventListener('scroll', handleScroll);
+
+
+//rajouter une 3eme etape 
